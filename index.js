76c6fb624d055af9702c105840744fb324c3db19
Nagano Aya <x18086@osaka-geidai.ac.jp>
2019-10-10 15:05:26 +0900

    2日目1

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
+$(function () {
+	
+	var page = 0;
+
+	
+	var lastPage = parseInt($("#slide img").length - 1);
+
+	
+	$("#slide img").css("display", "none");
+
+	
+	$("#slide img").eq(page).css("display", "block");
+
+	
+	function changePage() {
+		$("#slide img").fadeOut(1000);
+		$("#slide img").eq(page).fadeIn(1000);
+	};
+
+	
+	var Timer;
+
+	function startTimer() {
+		Timer = setInterval(function () {
+			if (page === lastPage) {
+				page = 0;
+				changePage();
+			} else {
+				page++;
+				changePage();
+			};
+		}, 5000);
+	}
+	
+	function stopTimer() {
+		clearInterval(Timer);
+	}
+
+	
+	startTimer();
+});
+
+
 jQuery(function () {
 	jQuery(window).scroll(function () {
 		jQuery('.fadein').each(function () {
